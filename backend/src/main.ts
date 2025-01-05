2e7e4732032e597e66d5b29b19fcc75305010055
jhosep98 <jhosep149@hotmail.com>
2025-01-05 11:09:27 -0300

    feat(back): prisma and postgres config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+
+const DEFAULT_PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.use(cookieParser());
+  app.enableCors();
+  app.setGlobalPrefix('/v1/api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('EASY_TURNOS')
+    .setDescription('EASY_TURNOS API')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/v1/api', app, document);
+
+  await app.listen(process.env.PORT ?? DEFAULT_PORT);
 }
 bootstrap();
