e34b777d34e5f96fa1a4422868f68efe8c5111bc
jhosep98 <jhosep149@hotmail.com>
2025-01-04 22:46:22 -0300

    styles(front): normalize styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./i18n";
+// Globals Styles
 import "./index.css";
+import "./styles/normalize.css";
+
+import "./i18n";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
